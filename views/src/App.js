65e6704adee78accc06e9d9132c045fa9b531ecd
Idir6380 <idirbellili15@gmail.com>
2021-11-08 23:07:35 +0100

    first commit

---
+++ views/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import SkillsPage from './Component/pages/Skills/SkillsPage';
+
+function App() {
+  return (
+    <div>
+      <SkillsPage />
+
+    </div>
+  );
+}
+
+export default App;
